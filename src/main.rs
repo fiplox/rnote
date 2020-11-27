fefd1021015acc75f4bcc98b785459a6ee79ffee
Volodymyr Patuta <6977238-fiplox@users.noreply.gitlab.com>
2020-11-27 21:00:07 +0100

    minimal main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-mod notes;
+use anyhow::Result;
+use rnote::{app, process};
 
-fn main() {
-    println!("Hello, world!");
+mod rnote;
+
+fn main() -> Result<()> {
+    let mut app = app::make_app();
+
+    match rnote::app::make_app().get_matches().subcommand() {
+        ("new", Some(m)) => process::new(m)?,
+        ("remove", Some(m)) => process::remove(m)?,
+        ("edit", Some(m)) => process::edit(m)?,
+        ("list", Some(m)) => process::list(m)?,
+        ("search", Some(m)) => process::search(m)?,
+        _ => app.print_long_help()?,
+    };
+
+    Ok(())
 }
